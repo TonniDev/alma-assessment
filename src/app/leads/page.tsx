c7f9b4e33fd80e2994cf884aa9d926b29ddb3f99
Tonni Andrade <tonni.andrade@gmail.com>
2025-01-26 04:02:56 -0300

    [FIX] Add missing suspense wrapper

--- src/app/leads/page.tsx
+++ src/app/leads/page.tsx
@@ -1,14 +1,19 @@
 import { LeadsFilters } from '@app/leads/components/LeadsFilters';
 import { LeadsTable } from '@app/leads/components/LeadsTable';
 import { Flex } from '@chakra-ui/react';
+import { Suspense } from 'react';
 
 export default async function LeadsPage() {
   return (
     <Flex direction="column" gap={4} justify="flex-start" alignItems="flex-start" p={8} w="100%">
       <Flex>
-        <LeadsFilters />
+        <Suspense fallback={<div>Loading filters...</div>}>
+          <LeadsFilters />
+        </Suspense>
       </Flex>
-      <LeadsTable />
+      <Suspense fallback={<div>Loading data...</div>}>
+        <LeadsTable />
+      </Suspense>
     </Flex>
   );
 }
